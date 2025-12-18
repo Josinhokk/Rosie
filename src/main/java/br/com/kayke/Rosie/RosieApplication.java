2c89793505711f160ecb66fbe1ba54239d8c7d3a
Kayke <kaykeflausino@hotmail.com>
2025-12-18 09:20:00 -0300

    inicio do projeto

---
+++ src/main/java/br/com/kayke/Rosie/RosieApplication.java
@@ -0,0 +1,13 @@
+package br.com.kayke.Rosie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RosieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RosieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/kayke/Rosie/RosieApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kayke.Rosie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RosieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
